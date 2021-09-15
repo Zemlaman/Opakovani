88a675838b8cc0b49daa26b96ffb53230a3592f4
Matěj Žemlička <43809215+Zemlaman@users.noreply.github.com>
2021-09-15 21:42:10 +0200

    Add files via upload

---
+++ Bin.java
@@ -0,0 +1,25 @@
+package educanet;
+
+import java.util.Scanner;
+
+public class Bin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadejte cislo.");
+        int nmb = sc.nextInt();
+        int y = 0;
+        int[] bin = new int[100];
+        int index = 0;
+
+        while(nmb > 0) {
+            bin[index++] = nmb % 2;
+            nmb = nmb/2;
+            y++;
+        }
+
+        for(int i = y-1; i >= 0; i--){
+            System.out.print(bin[i]);
+        }
+
+    }
+}
